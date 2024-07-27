7f92a9c273b6af689779d6c6498ee98988e9df60
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2024-07-27 15:04:23 +0200

    initial setup

---
+++ app/success/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuccessPage = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-semibold text-primary">Success!</h1>
+    </div>
+  );
+};
+
+export default SuccessPage;
